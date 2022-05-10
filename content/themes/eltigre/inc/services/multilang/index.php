10e015524f0450c1be33b3a48d93681b06eec795
Jullian Leleux <jullian@eltigre.fr>
2022-05-10 10:26:31 +0200

    Get Geotargetly URL from .env

--- content/themes/eltigre/inc/services/multilang/index.php
+++ content/themes/eltigre/inc/services/multilang/index.php
@@ -66,12 +66,13 @@ class Multilang extends Service {
     }
 
     public static function add_geotargetly_script() { 
-        // if ( ! defined( 'GEOTARGETLY_URL' ) ) return; ?>
+        $geotargetly_url = getenv( 'GEOTARGETLY_URL' );
+         if ( empty( $geotargetly_url ) ) return; ?>
 
         <script>
             (function(g,e,o,t,a,r,ge,tl,y){
             t=g.getElementsByTagName(e)[0];y=g.createElement(e);y.async=true;
-            y.src='https://g1386590346.co/gl?id=-MvJcPYIhpDg1fd7q67Q&refurl='+g.referrer+'&winurl='+encodeURIComponent(window.location);
+            y.src=<?php echo $geotargetly_url; ?>'&refurl='+g.referrer+'&winurl='+encodeURIComponent(window.location);
             t.parentNode.insertBefore(y,t);
             })(document,'script');
         </script>
