03f27e02936e904be3898e87ee501ce5edc8180e
Jullian Leleux <jullian@eltigre.fr>
2022-05-10 10:35:11 +0200

    Segment key from .env

--- content/themes/eltigre/functions.php
+++ content/themes/eltigre/functions.php
@@ -261,11 +261,11 @@
 
 add_action( 'wp_head', 'init_segment', 99 );
 function init_segment() { 
-    if ( ! function_exists( 'getenv' ) || empty( getenv( 'SEGMENT_WRITE_KEY' ) ) ) return null; 
+    if ( ! function_exists( 'getenv' ) || empty( getenv( 'SEGMENT_KEY' ) ) ) return null; 
 	?>
     <script>
         !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="YOUR_WRITE_KEY";analytics.SNIPPET_VERSION="4.13.2";
-        analytics.load("<?php echo getenv( 'SEGMENT_WRITE_KEY' ); ?>");
+        analytics.load("<?php echo getenv( 'SEGMENT_KEY' ); ?>");
         analytics.page();
         }}();
     </script><?php
