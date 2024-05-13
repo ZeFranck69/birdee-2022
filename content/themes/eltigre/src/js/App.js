9441415576ab5bf37f00374209eef5a01a58ced6
ZeFrancky <francois.sarin.fr@gmail.com>
2024-05-13 11:43:36 +0200

    Add escape function on all href twig

--- content/themes/eltigre/src/js/App.js
+++ content/themes/eltigre/src/js/App.js
@@ -121,3 +121,22 @@ menusFooter.forEach((menuFooter) => {
 		oo_cm().consoleTimeEnd();
 	} catch (e) {}
 } /*eslint unicorn/no-abusive-eslint-disable:,eslint-comments/disable-enable-pair:,eslint-comments/no-unlimited-disable:,eslint-comments/no-aggregating-enable:,eslint-comments/no-duplicate-disable:,eslint-comments/no-unused-disable:,eslint-comments/no-unused-enable:,*/
+
+// Disable right-click
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
